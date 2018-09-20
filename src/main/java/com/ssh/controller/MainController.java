ee8d5311e260a213d93bf0c732f8af317f69ff73
XuDong <264835229@qq.com>
2018-09-20 18:23:02 +0800

    正好log4j

--- src/main/java/com/ssh/controller/MainController.java
+++ src/main/java/com/ssh/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.ssh.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,22 +11,29 @@ import com.ssh.service.TestService;
 @Controller
 public class MainController {
 	
+//	private static final Log log = LogFactory.getLog(MainController.class);
+	private static Logger log = Logger.getLogger(MainController.class);  
+	
 	@Autowired
     private TestService testService;
 
-    @RequestMapping(value = "test", method = RequestMethod.GET)
-    public String test(){
+    @RequestMapping(value = "test.do", method = RequestMethod.GET)
+    public String test() throws Exception{
 //        实际返回的是views/test.jsp ,spring-mvc.xml中配置过前后缀
-        return "test";
+    		log.info("这是info信息");
+    		log.error("这是error信息");
+    		log.debug("这是debug信息");
+    		int a = 1/0;
+    		return "test";
     }
     
-    @RequestMapping(value = "index", method = RequestMethod.GET)
+    @RequestMapping(value = "index.do", method = RequestMethod.GET)
     public String index(){
 //        实际返回的是views/test.jsp ,spring-mvc.xml中配置过前后缀
     	return "index";
     }
     
-    @RequestMapping(value = "springtest", method = RequestMethod.GET)
+    @RequestMapping(value = "springtest.do", method = RequestMethod.GET)
     public String springTest(){
     	System.out.println(testService.test());
         return "test";
